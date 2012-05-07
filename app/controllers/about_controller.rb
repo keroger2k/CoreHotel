090a9e946c4ed085d444e8d35ac4dfd025fc81d0
Kyle Rogers <kyle.rogers@gmail.com>
2012-05-07 16:01:00 -0500

    initial

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,14 @@
+class AboutController < ApplicationController
+
+	def index
+	end
+	
+	def about
+    end
+
+    def contact
+    end
+
+    def help
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoreHotel::Application.config.secret_token = 'aa95ec2bc17660c9731684de94783070e4214cb509e11fe177a2d9570f7b10571e2625bc572c70dcdf672e86b0e15bd81da2b4d8734824eb9594be28627a2676'
